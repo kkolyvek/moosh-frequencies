dceeec0d177e5175efdb217021e7037c147108d8
Koppi Kolyvek <kk674@cornell.edu>
2021-09-07 06:02:41 -0700

    refactored slope calculator

--- displayVid.py
+++ displayVid.py
@@ -25,8 +25,12 @@ def display_vid(vidObj):
     cv2.resizeWindow('analysis', 960, 540)
     cv2.moveWindow('analysis', 10, 540)
 
-    slope_left = []
-    slope_right = []
+    slope_a = []
+    slope_b = []
+
+    # counter / arr to record centroids
+    counter = -1
+    centr_arr = []
     while(True):
         ret, frame = vidObj.read()
 
@@ -50,8 +54,6 @@ def display_vid(vidObj):
 
             cv2.drawContours(frame, contours, -1, (0, 0, 255), 1)
 
-            cx = 0  # average x value (for centroid approx)
-            cy = 0  # average y value (for centroid approx)
             c_target = []  # array of dictionaries of on-target contours
 
             for c in contours:
@@ -68,8 +70,7 @@ def display_vid(vidObj):
                     mu = cv2.moments(c)
 
                     # get centroid (+ 1e-5 to avoid div by zero)
-                    mc = (mu['m10'] / (mu['m00'] + 1e-5),
-                          mu['m01'] / (mu['m00'] + 1e-5))
+                    mc = (mu['m10'] / (mu['m00'] + 1e-5), mu['m01'] / (mu['m00'] + 1e-5))
 
                     contour_dictionary = {
                         "contour": c,
@@ -81,19 +82,6 @@ def display_vid(vidObj):
                     # record a target contour
                     c_target.append(contour_dictionary)
 
-                    # record center values
-                    # cx += center[0]
-                    # cy += center[1]
-
-            """
-            TODO
-            THIS IS GOOD STUFF HERE BUT:
-            - only grabs moment of one contour... this is fine for a starting point
-            - find a way to combine moments of multiple contours... contour addition? i dunno
-            - https://docs.opencv.org/4.5.1/dd/d49/tutorial_py_contour_features.html
-            ===========================================================================
-            """
-
             # If there is more than one contour, get weighted average of their locations
             averaged_centroid = None
             if len(c_target) > 1:
@@ -104,30 +92,33 @@ def display_vid(vidObj):
                 avg_x = 0
                 avg_y = 0
                 for i in range(len(c_target)):
-                    avg_x += (c_target[i]["centroid"][0] *
-                              (c_target[i]["enc_circle_rad"] / total_rad))
-                    avg_y += (c_target[i]["centroid"][1] *
-                              (c_target[i]["enc_circle_rad"] / total_rad))
+                    avg_x += (c_target[i]["centroid"][0] * (c_target[i]["enc_circle_rad"] / total_rad))
+                    avg_y += (c_target[i]["centroid"][1] * (c_target[i]["enc_circle_rad"] / total_rad))
                 averaged_centroid = (avg_x, avg_y)
-            else:
+
+                centr_arr.append(averaged_centroid)
+                counter += 1
+            elif len(c_target) == 1:
                 averaged_centroid = c_target[0]["centroid"]
 
+                centr_arr.append(averaged_centroid)
+                counter += 1
+            else:
+                averaged_centroid = centr_arr[counter]
+
+                centr_arr.append(averaged_centroid)
+                counter += 1
+
             # Display centroids of all contours
-            cv2.circle(frame, (int(averaged_centroid[0]), int(
-                averaged_centroid[1])), 4, (0, 255, 0), -1)
+            cv2.circle(frame, (int(averaged_centroid[0]), int(averaged_centroid[1])), 4, (0, 255, 0), -1)
 
-            """
-            ===========================================================================
-            """
-
+            # Find two furthest tuples.
             tuple_a = averaged_centroid
             tuple_a_dist = 0
-            # Find two furthest tuples.
             for c in c_target:
                 for tuple in c["contour"]:
                     # loop through, find tuple furthest from center
-                    tuple_dist = math.sqrt(
-                        ((averaged_centroid[0]-tuple[0][0]) ** 2) + ((averaged_centroid[1]-tuple[0][1]) ** 2))
+                    tuple_dist = math.sqrt(((averaged_centroid[0]-tuple[0][0]) ** 2) + ((averaged_centroid[1]-tuple[0][1]) ** 2))
                     if tuple_dist > tuple_a_dist:
                         tuple_a = tuple[0]
                         tuple_a_dist = tuple_dist
@@ -137,62 +128,44 @@ def display_vid(vidObj):
             for c in c_target:
                 for tuple in c["contour"]:
                     # loop through, find tuple furthest from first tuple
-                    tuple_dist = math.sqrt(
-                        ((tuple_a[0]-tuple[0][0]) ** 2) + ((tuple_a[1]-tuple[0][1]) ** 2))
+                    tuple_dist = math.sqrt(((tuple_a[0]-tuple[0][0]) ** 2) + ((tuple_a[1]-tuple[0][1]) ** 2))
                     if tuple_dist > tuple_b_dist:
                         tuple_b = tuple[0]
                         tuple_b_dist = tuple_dist
 
             # draw tuples as points
-            cv2.circle(
-                frame, (int(tuple_a[0]), int(tuple_a[1])), 10, (255, 0, 0), -1)
-            cv2.circle(
-                frame, (int(tuple_b[0]), int(tuple_b[1])), 10, (255, 0, 0), -1)
+            cv2.circle(frame, (int(tuple_a[0]), int(tuple_a[1])), 10, (255, 0, 0), -1)
+            cv2.circle(frame, (int(tuple_b[0]), int(tuple_b[1])), 10, (255, 0, 0), -1)
 
-            # draw new enclosing circle + center
-            # radius = math.sqrt(
-            #     ((center[1]-tuple_a[1]) ** 2) + ((center[0]-tuple_a[0]) ** 2))
-            # cv2.circle(gray_invert, (int(center[0]), int(
-            #     center[1])), int(radius), (255, 255, 255), 3)
-            # cv2.circle(frame, (int(center[0]), int(
-            #     center[1])), int(radius), (0, 0, 0), 3)
-            # cv2.circle(frame, (int(center[0]), int(
-            #     center[1])), 10, (0, 0, 0), 3)
-
-            # display some of the info onto the image
-            # centertxt = 'Enclosing Circle Center: ' + \
-            #     str(averaged_centroid[0]) + ',' + str(averaged_centroid[1])
-            # cv2.putText(gray_invert, centertxt, (75, 150),
-            #             cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 4, cv2.LINE_4)
-            # radtxt = 'Enclosing Circle Radius: ' + str(radius) + ' px'
-            # cv2.putText(gray_invert, radtxt, (75, 250),
-            #             cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 4, cv2.LINE_4)
+            # draw lines between tuples
+            cv2.line(frame, (int(tuple_a[0]), int(tuple_a[1])), (int(averaged_centroid[0]), int(averaged_centroid[1])), (255, 255, 255), 5)
+            cv2.line(frame, (int(tuple_b[0]), int(tuple_b[1])), (int(averaged_centroid[0]), int(averaged_centroid[1])), (255, 255, 255), 5)
 
             # take slopes of lines between head and center, center and tail
-            # if (cy-tuple_left[1])/(cx-tuple_left[0]) != 0:
-            #     slope_left.append((cy-tuple_left[1])/(cx-tuple_left[0]))
-            # else:
-            #     slope_left.append(0)
-            # if (tuple_right[1]-cy)/(tuple_right[0]-cx) != 0:
-            #     slope_right.append((tuple_right[1]-cy)/(tuple_right[0]-cx))
-            # else:
-            #     slope_right.append(0)
-            # cv2.line(frame, (tuple_a[0], tuple_a[1]),
-            #          (int(center[0]), int(center[1])), (255, 255, 255), 7)
-            # cv2.line(frame, (tuple_b[0], tuple_b[1]),
-            #          (int(center[0]), int(center[1])), (255, 255, 255), 7)
+            if averaged_centroid[0] == tuple_a[0]:
+                slope_a.append(0)
+            else:
+                slope_a.append((averaged_centroid[1]-tuple_a[1]) / (averaged_centroid[0]-tuple_a[0]))
+
+            if averaged_centroid[0] == tuple_b[0]:
+                slope_b.append(0)
+            else:
+                slope_b.append((tuple_b[1]-averaged_centroid[1]) / (tuple_b[0]-averaged_centroid[0]))
 
             cv2.imshow('orig', frame)
             cv2.imshow('analysis', gray_invert)
 
             # Press Q on keyboard to stop recording
-            if cv2.waitKey(0) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
         # Break the loop
         else:
             print('Read error...')
             break
+
+    print(slope_a)
+    print(slope_b)
 
     # When everything done, release the video capture and video write objects
     vidObj.release()
